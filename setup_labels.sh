c19d30418ed15cb4337340a8f0d08a336f3bcb1c
roodolv <113752412+roodolv@users.noreply.github.com>
2025-09-01 16:07:08 +0900

    feat(metadata): added improvement label to action.yml

--- setup_labels.sh
+++ setup_labels.sh
@@ -5,6 +5,7 @@ gh label create release -d "Create release with this label" -c 00DD55
 
 # If necessary, these labels should also be created.
 # gh label create feature -d "New feature etc" -c A2EEEF \
+# && gh label create improvement -d "Improvements to existing functionality and etc" -c ED650E \
 # && gh label create hotfix -d "Small, urgent patches other than usual updates" -c FF5075 \
 # && gh label create breaking-change -d "Something could break existing usage" -c 110000 \
 # && gh label create skip-changelog -d "This will not be added to CHANGELOG" -c BFDADC
